f91bf42b4f18e261dee77e55a5c64edc3934cdfc
0x6d61 <0x6d61@gmail.com>
2021-02-10 00:33:48 +0900

    独自のワードを定義できるように

--- forth.rb
+++ forth.rb
@@ -13,7 +13,7 @@ class Forth
 
   def run
     @src.each_line do |line|
-        @pc = 0
+      @pc = 0
       @tokens = line.split(" ")
       while @tokens.size > @pc
         eval(@tokens[@pc])
@@ -43,7 +43,7 @@ class Forth
       y, x = pop, pop
       push(x % y)
     elsif token == "/MOD"
-      y,x = pop,pop
+      y, x = pop, pop
       z = x % y
       a = x / y
       push(z)
@@ -53,7 +53,7 @@ class Forth
       push(x.abs)
     elsif token == "NEGATE"
       x = pop
-      push(- x)
+      push(-x)
     elsif token == "DROP"
       drop
     elsif token == "DUP"
@@ -72,10 +72,47 @@ class Forth
     elsif token == "2OVER"
     elsif token == "2DUP"
       dup2
+    elsif token == "="
+      y, x = pop, pop
+      x == y ? push(-1) : push(0)
+    elsif token == "<>"
+      y, x = pop, pop
+      x != y ? push(-1) : push(0)
+    elsif token == "<"
+      y, x = pop, pop
+      x < y ? push(-1) : push(0)
+    elsif token == ">"
+      y, x = pop, pop
+      x > y ? push(-1) : push(0)
+    elsif token == "<="
+      y, x = pop, pop
+      x <= y ? push(-1) : push(0)
+    elsif token == ">="
+      y, x = pop, pop
+      x >= y ? push(-1) : push(0)
+    elsif token == "AND"
+      push(pop & pop)
+    elsif token == "OR"
+      push(pop | pop)
+    elsif token == "XOR"
+      push(pop ^ pop)
+    elsif token == ":"
+      word = []
+      @pc += 1
+      word_name = @tokens[@pc]
+      while true
+        @pc += 1
+        break if @tokens[@pc] == ";"
+        word.push(@tokens[@pc])
+        if @pc > @tokens.size
+          raise ProgramError, "ワード定義 :に対応する;が見つかりません。"
+        end
+      end
+      @word[word_name] = word
     elsif token == "."
       print "#{pop} "
     elsif token == "CR"
-        puts ""
+      puts ""
     elsif token == ".S"
       print ".S <#{@stack.size}> "
       @stack.map { |s| print "#{s} " }
@@ -83,16 +120,23 @@ class Forth
       @pc = @tokens.size
     elsif token == "("
       while true
-        @pc+=1
+        @pc += 1
         if @tokens[@pc] == ")"
           return
         end
         if @pc > @tokens.size
-          raise ProgramError,"コメント (に対応する)が見つかりません。"
+          raise ProgramError, "コメント (に対応する)が見つかりません。"
         end
-      end      
+      end
+    elsif @word.include?(token)
+      @tokens = [*@word[token], *@tokens.slice(@pc + 1, @tokens.size)]
+      @pc = 0
+      while @tokens.size > @pc
+        eval(@tokens[@pc])
+        @pc += 1
+      end
     else
-      raise ProgramError, "存在しない命令です。"
+      raise ProgramError, "存在しない命令(#{token})です。"
     end
   end
 
@@ -112,15 +156,15 @@ class Forth
   end
 
   def nip
-    [*@stack.slice(0,@stack.size-2),stack.slice(-1)]
+    [*@stack.slice(0, @stack.size - 2), stack.slice(-1)]
   end
 
   def swap
-    y,x = pop,pop
+    y, x = pop, pop
     push(y)
     push(x)
   end
-  
+
   def over
     push(@stack[-2])
   end
@@ -130,14 +174,13 @@ class Forth
   end
 
   def drop2
-    pop;pop
+    pop; pop
   end
 
   def dup2
     push(@stack[-2])
     push(@stack[-2])
   end
-
 end
 
 if $0 == __FILE__
@@ -147,5 +190,3 @@ if $0 == __FILE__
     Forth.run(File.open(ARGV[0]).read)
   end
 end
-
-
